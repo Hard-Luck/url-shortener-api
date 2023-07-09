bdc4ac509811648bda006864bdea0716941ba720
Mark Tyree <tyreemarka@gmail.com>
2023-07-09 17:53:12 +0100

    cors

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import express from 'express';
 import apiRouter from './routers/api';
+import cors from 'cors';
+
+
 
 const app = express();
+app.use(cors());
 app.use(express.json());
-
 app.use('/', apiRouter);
-
+app.get('/:url_id')
 export default app;
